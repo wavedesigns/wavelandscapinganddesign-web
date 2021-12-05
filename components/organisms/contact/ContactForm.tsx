7d17321170d52eab81b83d4d86c5b2eb914e6aa7
Peter Tumulty <ptumulty923@gmail.com>
2021-12-05 13:33:48 -0500

    removed all of kwes forms

--- components/organisms/contact/ContactForm.tsx
+++ components/organisms/contact/ContactForm.tsx
@@ -1,22 +1,17 @@
 import Button from 'components/atoms/Button'
 import InputText from 'components/atoms/InputText'
-import kwesforms from 'kwesforms'
-import React, { useEffect } from 'react'
 
 interface Props {
   isBlue: boolean
 }
 const ContactForm = ({ isBlue }: Props) => {
-  useEffect(() => kwesforms.init(), [])
   return (
     <>
       <div className="space-y-3 sm:space-y-4 w-full">
         <p className="text-xl font-serif">
           Or fill out the form below, and we&lsquo;ll get back to you shortly!
         </p>
-        <form
-          className={`kwes-form space-y-3 sm:space-y-4 ${isBlue ? 'text-white' : 'text-darkBlue'}`}
-        >
+        <form className={`space-y-3 sm:space-y-4 ${isBlue ? 'text-white' : 'text-darkBlue'}`}>
           <InputText id="full-name" label="Full Name" type="text" />
           <InputText id="email" label="Email" type="email" />
           <InputText id="phone" label="Phone" type="text" />
