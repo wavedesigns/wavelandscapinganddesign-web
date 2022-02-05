d5b6b7b999efda679bbb430c982d9553217e7ca2
Peter Tumulty <ptumulty923@gmail.com>
2022-02-04 21:50:37 -0500

    bug: removed commas from sitemap

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -15,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
       <priority>1.0</priority>
     </url> 
 `
-  ).join(', ')
+  ).join('')
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
