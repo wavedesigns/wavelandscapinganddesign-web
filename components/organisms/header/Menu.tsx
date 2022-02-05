9326f21172a4ad33d0247dbf7a9c92989817ea2a
Peter Tumulty <ptumulty923@gmail.com>
2022-02-04 21:48:18 -0500

    added height to nav items

--- components/organisms/header/Menu.tsx
+++ components/organisms/header/Menu.tsx
@@ -1,8 +1,9 @@
-import MobileMenu from 'components/molecules/MobileMenu'
 import Navigation from 'components/molecules/Navigation'
 import SocialMedia from 'components/molecules/SocialMedia'
+import dynamic from 'next/dynamic'
 import React from 'react'
 
+const MobileMenu = dynamic(() => import('components/molecules/MobileMenu'))
 const Menu = () => (
   <>
     <div className="hidden lg:block">
